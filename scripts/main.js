c57211b2fd83ede306ac137b96011e2a9a79a19f
momcheva <nora.momcheva@gmail.com>
2016-09-02 14:07:25 +0300

    dropdown menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,14 @@
 
 
 
+$(document).ready(function() {
+    $('.sub-menu').prev('a.single-menu-element-link').append('<span class="indicator"></span>');
 
-/*$(document).ready(function() {
-    $('.sub-menu').prev('a').append('<span class="indicator"></span>');
-
-        $('.single-menu-element:has(.sub-menu)').hover(function() {
+        $('li.single-menu-element:has(.sub-menu)').hover(function() {
             $(this).find('a .indicator').addClass('hover-indicator');
             $(this).find('ul:first').slideDown('fast').addClass('active');
         }, function () {
             $(this).find('a .hover-indicator').removeClass('hover-indicator');
             $(this).children('ul.active').slideUp('fast').removeClass('active');
         });
-
-});*/
-
-function callback() {
-    $(".menu li a").append("<span></span>");
-}
-$(document).ready(function() {
-    //window.setTimeout(callback, 100);
-    callback();
 });
