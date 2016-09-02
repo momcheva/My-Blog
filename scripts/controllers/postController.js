7e744710a253470f1f9d30a2ee987b43a3e56939
momcheva <nora.momcheva@gmail.com>
2016-09-02 10:00:35 +0300

    Changes in design

--- scripts/controllers/postController.js
+++ scripts/controllers/postController.js
@@ -1,13 +1,18 @@
 class PostController {
-    constructor (postView,requester,baseUrl, appKey) {
+    constructor(postView, requester, baseUrl, appKey) {
         this._postView = postView;
         this._requester = requester;
         this._appKey = appKey;
         this._baseServiceUrl = baseUrl + "/appdata/" + appKey + "/posts/";
     }
-    showCreatePostPage(data, isLoggedIn){
+
+    showCreatePostPage(data, isLoggedIn) {
         this._postView.showCreatePostPage(data, isLoggedIn);
     }
+
+ /*   showSinglePost(data, isLoggedIn) {
+        this._postView.showSinglePost(data, isLoggedIn);
+    }*/
 
     createNewPost(data) {
 
@@ -21,13 +26,48 @@ class PostController {
         }
 
         this._requester.post(this._baseServiceUrl, data,
-            function success (responseData) {
-            showPopup('success', 'Post created');
-            redirectUrl('#/');
+            function success(responseData) {
+                showPopup('success', 'Post created');
+                redirectUrl('#/');
             },
-            function error (responseData){
-            showPopup('error', 'An error has occurred while attempting to create a new post.');
+            function error(responseData) {
+                showPopup('error', 'An error has occurred while attempting to create a new post.');
+
+            });
+    }
+
+    editPost(data) {
+
+    }
+
+    deletePost(postId) {
+
+    }
+
+
+/*    // Attach a submit handler to the form
+    $(document).ready(function(){
+        $( "#searchForm" ).submit(function( event ) {
+
+    // Stop form from submitting normally
+    event.preventDefault();
+
+    // Get some values from elements on the page:
+    var $form = $( this ),
+        term = $form.find( "input[name='s']" ).val(),
+        url = $form.attr( "action" );
+
+    // Send the data using post
+    var posting = $.post( url,{s: term});
 
         });
-    }
+
+    // Put the results in a div
+    posting.done(function( data ) {
+        var content = $( data ).find( "#content" );
+        $( "#result" ).empty().append( content );
+      });
+    });
+    */
+    
 }
--- scripts/views/postView.js
+++ scripts/views/postView.js
@@ -3,37 +3,38 @@ class PostView {
         this._mainContentSelector = mainContentSelector;
         this._wrapperSelector = wrapperSelector
     }
-    showCreatePostPage(data, isLoggedIn){
+
+    showCreatePostPage(data, isLoggedIn) {
         let _that = this;
 
         let requestTemplate;
 
-        if(isLoggedIn){
+        if (isLoggedIn) {
             requestTemplate = "templates/form-user.html";
         }
-        else{
+        else {
             requestTemplate = "templates/form-guest.html";
         }
 
-        $.get(requestTemplate, function(template){
+        $.get(requestTemplate, function (template) {
             let renderedTemplate = Mustache.render(template, null);
 
             $(_that._wrapperSelector).html(renderedTemplate);
 
-            $.get('templates/create-post.html', function(template){
+            $.get('templates/create-post.html', function (template) {
                 let renderedLogin = Mustache.render(template, null);
                 $(_that._mainContentSelector).html(renderedLogin);
 
                 $('#author').val(data.fullName);
-                
-                $('#create-new-post-request-button').on('click', function(ev){
+
+                $('#create-new-post-request-button').on('click', function (ev) {
                     let title = $('#title').val();
                     let author = $('#author').val();
                     let content = $('#content').val();
                     let date = moment().format("MMM Do YYYY");
 
 
-                    let data= {
+                    let data = {
                         title: title,
                         content: content,
                         author: author,
@@ -41,10 +42,19 @@ class PostView {
                     };
 
                     triggerEvent('createNewPost', data);
-                })
+                }) 
             })
 
         });
     }
-    
+
+    showSinglePost() {
+            $('a.single-post-id').click(function (ev) {
+            alert('Hellow world');
+            ev.preventDefault();
+        });
+    }
 }
+
+    
+
